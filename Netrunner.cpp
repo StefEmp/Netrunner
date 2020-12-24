c3d894a4bf7fd50969704703ba8b7f7769337de0
StefEmp <stephane.emptage@hotmail.co.uk>
2020-12-24 18:17:52 +0000

    Implementing string output

--- Netrunner.cpp
+++ Netrunner.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+
 int main()
 {
+    std::cout  << "Welcome to Netrunner...";
+    std::cout <<std::endl;
+    std::cout  << "You exist between the lines of reality and cyberspace a blemish on the corporate world around you"; 
+    std::cout <<std::endl;
+    std::cout  << "This is your time, the time to wrench back humanity from the clutches of the corporations and bring them to their knees...";
+    std::cout <<std::endl;
+    std::cout  << "To do this, you will need to hack into the megacorp mainframe, using the correct codes to destroy them once and for all!";
+    std::cout <<std::endl;         
     return 0;
 }
