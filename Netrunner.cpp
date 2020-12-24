88c1fa5592d02405d31265a5e311bec78d45e7c6
StefEmp <stephane.emptage@hotmail.co.uk>
2020-12-24 21:18:19 +0000

    Tidied up code

--- Netrunner.cpp
+++ Netrunner.cpp
@@ -3,25 +3,12 @@
 int main()
 {
     //Game welcome message
-    std::cout  << "___________________________________________________________________________________________________________________________";
-    std::cout <<std::endl;  
-    std::cout  << "";
-    std::cout <<std::endl;
-    std::cout  << "Welcome to Netrunner - A Digital Revolution";
-    std::cout <<std::endl;
-    std::cout  << "___________________________________________________________________________________________________________________________";
-    std::cout <<std::endl;  
-    std::cout  << "" <<std::endl;
-    std::cout  << "" <<std::endl;
-    std::cout  << "You exist between the lines of reality and cyberspace a blemish on the corporate world around you."; 
-    std::cout <<std::endl;
-    std::cout  << "" <<std::endl;
-    std::cout  << "This is your time, the time to wrench back humanity from the clutches of the corporations and bring them to their knees...";
-    std::cout <<std::endl;
-    std::cout  << "" <<std::endl;
-    std::cout  << "To do this, you will need to hack into the megacorp mainframe, using the correct codes to destroy them once and for all!";
-    std::cout <<std::endl;    
-    std::cout  << "";
+    std::cout  << "______________________________________________________________________________________________________________________\n\n\n";
+    std::cout  << "Welcome to Netrunner - A Digital Revolution\n";
+    std::cout  << "______________________________________________________________________________________________________________________\n\n\n";
+    std::cout  << "You exist between the lines of reality and cyberspace a blemish on the corporate world around you.\n\n"; 
+    std::cout  << "It's time to wrench back humanity from the clutches of the corporations and bring them to their knees...\n\n";
+    std::cout  << "To do this, you will need to hack into the megacorp mainframe, using the correct codes to destroy them once and for all!\n\n\n";
 
 //declare our 3 number code
     const int CodeA = 4;
@@ -34,35 +21,33 @@ int main()
     const int CodeProduct = CodeA * CodeB * CodeC;
 
 //print our sum and product to the terminal
-    std::cout <<std::endl;
-    std::cout  << "" <<std::endl;
-    std::cout << "+ The code contains 3 numbers" << std::endl;
-    std::cout  << "" <<std::endl;
-    std::cout << "+ The code will add up to: " << CodeSum <<std::endl;
-    std::cout  << "" <<std::endl;
-    std::cout << "+ The codes multiplied by each other with equal: " << CodeProduct <<std::endl;
-    std::cout  << "" <<std::endl;
-    std::cout << "-- Now input each number in turn: " << CodeProduct <<std::endl;
-    std::cout  << "" <<std::endl;
-    std::cout  << "" <<std::endl;
 
+  
+    std::cout << "+ The code contains 3 numbers\n\n";
+    std::cout << "+ The code will add up to: " << CodeSum << "\n\n";
+    std::cout << "+ The codes multiplied by each other will equal: " << CodeProduct << "\n\n";
+    std::cout  << "______________________________________________________________________________________________________________________\n\n";
+    std::cout << "- Now input each number in turn:\n\n";
+
+//setting up user guess and user input and store it
     int GuessA, GuessB, GuessC;
     
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
+
    
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+//if statements to say whether user has won or lost
+
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
-    {
-        std::cout << "Nice hacking my friend, your one step closer to destroying the corporations for good!";
-    }
+        {
+            std::cout << "\nNice hacking my friend, your one step closer to destroying the corporations for good!\n";
+        }
     else
     {
         {
-            std::cout << "Your hacking skills have failed you young padawan, have another go before it's too late and they catch on to you!";
+            std::cout << "\nYour hacking skills have failed you young padawan, have another go before it's too late and they catch on to you!\n";
         }
     }
     
