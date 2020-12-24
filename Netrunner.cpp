e0124fd67441344a09bf3c5fde4ef89fd47f7997
StefEmp <stephane.emptage@hotmail.co.uk>
2020-12-24 18:34:24 +0000

    Basic use of int variables alongside printing

--- Netrunner.cpp
+++ Netrunner.cpp
@@ -2,13 +2,41 @@
 
 int main()
 {
-    std::cout  << "Welcome to Netrunner...";
+    std::cout  << "___________________________________________________________________________________________________________________________";
+    std::cout <<std::endl;  
+    std::cout  << "";
     std::cout <<std::endl;
-    std::cout  << "You exist between the lines of reality and cyberspace a blemish on the corporate world around you"; 
+    std::cout  << "Welcome to Netrunner - A Digital Revolution";
     std::cout <<std::endl;
+    std::cout  << "___________________________________________________________________________________________________________________________";
+    std::cout <<std::endl;  
+    std::cout  << "";
+    std::cout <<std::endl;
+    std::cout  << "You exist between the lines of reality and cyberspace a blemish on the corporate world around you..."; 
+    std::cout <<std::endl;
+    std::cout  << "";
+    std::cout <<std::endl;  
     std::cout  << "This is your time, the time to wrench back humanity from the clutches of the corporations and bring them to their knees...";
     std::cout <<std::endl;
+    std::cout  << "";
+    std::cout <<std::endl;  
     std::cout  << "To do this, you will need to hack into the megacorp mainframe, using the correct codes to destroy them once and for all!";
-    std::cout <<std::endl;         
+    std::cout <<std::endl;    
+    std::cout  << "";
+
+    int a = 4;
+    int b = 3;
+    int c = 2;
+
+
+
+    int sum = a + b + c;
+
+    int product = a * b * c;
+
+    std::cout <<std::endl;
+    std::cout << sum <<std::endl;
+    std::cout << product <<std::endl;
+
     return 0;
 }
